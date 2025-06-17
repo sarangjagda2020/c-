c91bbca915b01bd0bac6c0c05783dc4265cb3bef
sarangjagda2020 <sarangjagda2020@gmail.com>
2025-06-18 01:34:50 +0530

    Add files via upload
    Using constant to give permanent values to variables

---
+++ constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+  using namespace std;
+
+int main()
+ {
+     const int a = 18;
+     int a = 45 ; 
+     cout<<"value of a is "<<a ;
+}
+
+//constant are used when we have to give permanent value means if coder make changes in value then the original value is already been given by using constant so there is no issue 
