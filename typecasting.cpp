7b8d9adc6b39355466cfa499130f4eb2ab8cb4e3
sarangjagda2020 <sarangjagda2020@gmail.com>
2025-06-18 01:22:26 +0530

    Add files via upload
    TYPECASTING = convert one datatype to another datatype

---
+++ typecasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+ {
+
+  	 float a = 12.445;
+    cout << "Value of a is "<<(int)a;
+    return 0;
+}
+
+//conver one datatype to another is basically called typecasting...
