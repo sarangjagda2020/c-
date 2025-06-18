812ba7f7dcf007f069d21a0025e71b62c0d190c6
sarangjagda2020 <sarangjagda2020@gmail.com>
2025-06-18 19:04:52 +0530

    Add files via upload
    Basic While Loop

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+ int i=1;
+    while(i<=40){
+
+         cout<<i<<endl;
+         i++;
+
+     }
+
+
+}
+
+//basic code of while loop in C++
