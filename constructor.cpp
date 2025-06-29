9653b31cb138210d2976524d67dc9e418eca5e95
Sarang Nitin Jagdale <sarangjagda2020@gmail.com>
2025-06-29 13:37:07 +0530

    Add files via upload
    Constructor - "A constructor is a special function in a class that:
    Automatically gets called when an object is created.
    Initializes object data members. "

---
+++ constructor.cpp
@@ -0,0 +1,23 @@
+// Creating a class Laptop that displays "Laptop is turned on" when an object is created.
+
+
+#include<iostream>
+using namespace std;
+
+ class laptop{
+
+    public :
+
+    laptop()
+    
+    {
+        cout<<"Laptop is On "<<endl ;
+
+
+    };
+};
+ int main () {
+
+    laptop asus ; 
+    return 0 ;
+ }
